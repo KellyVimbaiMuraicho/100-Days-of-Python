acd8349b2992c2a2b3ae4ed115f2c71ae20abad6
Kelly V Muraicho <muraichokelly@gmail.com>
2025-08-04 23:05:18 +0200

    per-person amount

--- D2/calculator.py
+++ D2/calculator.py
@@ -6,6 +6,8 @@ tip = int(input("How much tip would you like to give ? 10,12, or 15 "))
 
 people = int ( input("How many people to split the bill ?"))
 
-bill_with_tip = tip/100 *bill + bill 
+bill_with_tip = tip/100 *bill + bill
 
-print(f"The total bill is : {bill_with_tip}")                     
+bill_per_person = bill_with_tip/people
+
+print(f"Each person will pay : {bill_per_person}")                     
