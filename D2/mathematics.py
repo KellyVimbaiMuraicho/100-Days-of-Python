31119b6f0c1a16f998d59182690290f2cdd6a80b
Kelly V Muraicho <muraichokelly@gmail.com>
2025-08-04 17:50:20 +0200

    mathematics py

---
+++ D2/mathematics.py
@@ -0,0 +1,15 @@
+# PEMDASLR parentheses---->exponents-->multiplication-->division-->subtraction
+# ()
+# **
+# *
+# /
+# +
+# -
+
+# print(round(57.5)) rounds off number
+
+score = 0 
+height = 1.8
+is_winning = True
+
+print(f"Your score is = {score},your height is {height}.You are winning is {is_winning}")
